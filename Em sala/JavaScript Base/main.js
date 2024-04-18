1b112e340033b0e7d72354a747c07a897184262e
Gabriel Maia <135539097+ConsidereSeGerenciado@users.noreply.github.com>
2024-04-18 09:55:42 -0300

    JS aula

---
+++ Em sala/JavaScript Base/main.js
@@ -0,0 +1,17 @@
+// demonstração de NaN
+let x = (0/0 - 'Itajubá');
+console.log(typeof(x));
+console.log(x);
+
+let nome2 = 'Itajubá';
+let nome3 = `Minha cidade $';
+{nome2} e é nota ${5+5}`;
+console.log(nome3);
+console.log(nome2.length);
+
+let literal = '2';
+console.log(typeof(literal));
+literal += 1;
+console.log(literal);
+literal++;
+console.log(literal);
